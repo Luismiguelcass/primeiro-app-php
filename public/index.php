069fa315084a1c7eeaeab2765da5c94c77631949
Luismiguelcass <273195@unifio.edu.br>
2025-09-18 15:59:00 +0000

    Feito

--- public/index.php
+++ public/index.php
@@ -1,17 +1,79 @@
-<?php
-require '../vendor/autoload.php';
-
-use Application\Hello;
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/default.css" rel="stylesheet" />
+    <title>Larika</title>
 </head>
 <body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./img/ham-ico-64.png" alt="" width="32" height="32">
+                Larika
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain"
+                aria-controls="navbarMain" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarMain">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home Page</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="menuLink" role="button" 
+                            data-bs-toggle="dropdown" aria-expanded="false">Cardapio</a>
+                        <ul class="dropdown-menu" aria-labelledby="menuLink">
+                            <li>
+                                <a class="dropdown-item" href="#">Hamburger</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Hot Dog</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Batatas Recheadas</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Petiscos</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Bebidas</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./img/hamburger-h500.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/hotdog-h500.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/potato-h500.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/snacks-h500.png" class="d-block" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
